a3f1abb906f0940b71e8598c601ef3c8d871706d
SaikiranYedugani <SaikiranYedugani@users.noreply.github.com>
2020-08-29 23:51:59 +0530

    Updates

---
+++ precache-manifest.2cc23f355a609a7469ab53183d64c66a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "883a8cc45f38670c8140",
+    "url": "/SaikiranYedugani/SaikiranYedugani.github.io/static/js/runtime~main.2f563397.js"
+  },
+  {
+    "revision": "556a7bb6b8bac4abd2ab",
+    "url": "/SaikiranYedugani/SaikiranYedugani.github.io/static/js/main.1e9e37d9.chunk.js"
+  },
+  {
+    "revision": "206892ec38c99bdd1291",
+    "url": "/SaikiranYedugani/SaikiranYedugani.github.io/static/js/2.521e897d.chunk.js"
+  },
+  {
+    "revision": "556a7bb6b8bac4abd2ab",
+    "url": "/SaikiranYedugani/SaikiranYedugani.github.io/static/css/main.cfebcb3f.chunk.css"
+  },
+  {
+    "revision": "967cbdab6a5c985f252ac423d3e168ba",
+    "url": "/SaikiranYedugani/SaikiranYedugani.github.io/index.html"
+  }
+];
